325177fbc35e38bac663745c72d8536188d45690
Nicolas Valenzuela <nicovalenzuela4@gmail.com>
2021-07-18 23:48:02 -0300

    segundo commit

--- api/routes/alarms.js
+++ api/routes/alarms.js
@@ -100,7 +100,7 @@ router.delete("/alarm-rule", checkAuth, async (req, res) => {
 //CREATE ALARM
 async function createAlarmRule(newAlarm) {
   try {
-    const url = "http://localhost:8085/api/v4/rules";
+    const url = "http://" + process.env.EMQX_NODE_HOST + ":8085/api/v4/rules";
 
     // topicExample = userid/did/temp  //msgExample = {value: 20}
     const topic =
@@ -152,7 +152,11 @@ async function createAlarmRule(newAlarm) {
         createTime: Date.now()
       });
 
-      const url = "http://localhost:8085/api/v4/rules/" + mongoRule.emqxRuleId;
+      const url =
+        "http://" +
+        process.env.EMQX_NODE_HOST +
+        ":8085/api/v4/rules/" +
+        mongoRule.emqxRuleId;
 
       const payload_templ =
         '{"userId":"' +
@@ -192,7 +196,11 @@ async function createAlarmRule(newAlarm) {
 //UPDATE ALARM STATUS
 async function updateAlarmRuleStatus(emqxRuleId, status) {
   try {
-    const url = "http://localhost:8085/api/v4/rules/" + emqxRuleId;
+    const url =
+      "http://" +
+      process.env.EMQX_NODE_HOST +
+      ":8085/api/v4/rules/" +
+      emqxRuleId;
 
     const newRule = {
       enabled: status
@@ -216,7 +224,11 @@ async function updateAlarmRuleStatus(emqxRuleId, status) {
 //DELETE ONLY ONE RULE
 async function deleteAlarmRule(emqxRuleId) {
   try {
-    const url = "http://localhost:8085/api/v4/rules/" + emqxRuleId;
+    const url =
+      "http://" +
+      process.env.EMQX_NODE_HOST +
+      ":8085/api/v4/rules/" +
+      emqxRuleId;
 
     const emqxRule = await axios.delete(url, auth);
 
