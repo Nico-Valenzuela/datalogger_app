08315c9ea52ba98263040e8c2c4edc55d0c16877
Nicolas Valenzuela <nicovalenzuela4@gmail.com>
2021-08-02 00:32:20 -0300

    tengo sueño

--- api/routes/emqxapi.js
+++ api/routes/emqxapi.js
@@ -168,7 +168,8 @@ global.check_mqtt_superuser = async function checkMqttSuperUser() {
 };
 
 setTimeout(() => {
+  console.log("LISTING RESORUCES!!!!!!!!!");
   listResources();
-}, 1000);
+}, process.env.EMQX_RESOURCES_DELAY);
 
 module.exports = router;
